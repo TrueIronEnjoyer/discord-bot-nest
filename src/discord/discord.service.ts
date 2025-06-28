00814b55a65b1182f1764b21fb9baa001471f865
TrueIronEnjoyer <azizumerov7@yandex.com>
2025-06-28 18:38:51 +0500

    modified:   .gitignore moved to discorde.service.ts:    src/discord/commands/add-coins.ts moved to discorde.service.ts:    src/discord/commands/coins.ts moved to discorde.service.ts:    src/discord/commands/top.ts added comands(top, coins, add-coins) in service class from src/discored/commands:   src/discord/discord.service.ts modified:   .gitignore

--- src/discord/discord.service.ts
+++ src/discord/discord.service.ts
@@ -1,12 +1,12 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { Client, GatewayIntentBits, VoiceState, Message, EmbedBuilder } from 'discord.js';
 import { PrismaClient } from '../../generated/prisma';
-import { AddCoins } from './commands/add-coins';
 import * as dotenv from 'dotenv';
 import { CreateUpdateUser } from './utils/create-update-user';
 import { CreateUserStatDto } from './dto/create-user-stat.dto';
-import { Coins } from './commands/coins';
-import { Top } from './commands/top';
+import { ReadUser } from './utils/read-user';
+import { MakeEmbeds } from './utils/make-embeds-users';
+import { ReadUsersTop } from './utils/read-users-top';
 
 dotenv.config();
 
@@ -50,13 +50,87 @@ export class DiscordService implements OnModuleInit {
     await CreateUpdateUser(userStat, this.prisma);
 
     if (message.content.startsWith('!coins')) {
-      Coins(message, this.prisma);
+      this.coins(message);
     } else if (message.content.startsWith('!top')) {
-      Top(message, this.prisma);
+      this.top(message);
     } else if (message.content.startsWith('!addcoins')) {
-      AddCoins(message, this.prisma);
+      this.addCoins(message);
     }
     } catch(err) {}
   }
+
+  private async addCoins(message: Message) {
+    if (message.guild?.ownerId != message.author.id) {
+        await message.reply('Эта команда доступна только владельцу сервера');
+        return;
+    }
+
+    const mentions = message.mentions.users;
+        
+    if (!mentions.size) {
+        await message.reply('Укажи пользователя: `!addcoins @user количество`');
+        return;
+    }
+    
+    const args = message.content.trim().split(/\s+/);
+    const amountStr = args[2];
+    const amount = parseInt(amountStr, 10);
+        
+    if (isNaN(amount)) {
+        await message.reply('Укажи количество SkamCoins: `!addcoins @user 100`');
+        return;
+    }
+        
+    const mentionedUser = mentions.first();
+    if (!mentionedUser) return;
+        
+    const userId = mentionedUser.id;
+    const serverId = message.guild?.id ?? 'global';
+
+    let userStat: CreateUserStatDto = {
+        userId: userId,
+        serverId: serverId,
+        coins: BigInt(amount),
+    };
+    
+    await CreateUpdateUser(userStat, this.prisma);
+
+    const updatedUser = await ReadUser(userId, serverId, this.prisma);
+
+    const embed: EmbedBuilder = await MakeEmbeds(message.client, [updatedUser], 'Добавлено SkamCoins');
+
+    await message.reply({ embeds: [embed] });
+  }
+
+  private async coins(message: Message) {
+    const mentions = message.mentions.users;
+    
+    let userId: string = message.author.id;
+    if (mentions.size) {
+        const mentionedUser = mentions.first();
+        if (!mentionedUser) return;
+    
+        userId = mentionedUser.id;
+    }
+
+    const serverId = message.guild?.id ?? 'global';
+
+    const user = await ReadUser(userId, serverId, this.prisma);
+    const embed: EmbedBuilder = await MakeEmbeds(message.client, [user], 'Баланс');
+    await message.reply({ embeds: [embed] });
+  }
+
+  private async top(message: Message) {
+    const args = message.content.trim().split(/\s+/);
+    const limit = parseInt(args[1], 10);
+    const topLimit = !isNaN(limit) && limit > 0 ? limit : 10;
+
+    const top = await ReadUsersTop(message.guildId || 'global', topLimit, this.prisma);
+    
+    const embed: EmbedBuilder = await MakeEmbeds(message.client, top, 'Топ SkamCoin владельцев');
+    
+    await message.reply({ embeds: [embed] });
+}
+
 
 }
